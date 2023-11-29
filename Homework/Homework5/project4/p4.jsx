898f38a6be9c84f6d141d56bd2fe1eb402049093
Dưn <115691542+realthuyduong2002@users.noreply.github.com>
2023-11-29 21:29:27 +0700

    Add files via upload

---
+++ Homework/Homework5/project4/p4.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dynamic from './components/dynamic/Dynamic';
+import Header from './components/header/Header';
+
+
+ReactDOM.render(
+  <div>
+    <Header />
+    <Dynamic />
+  </div>,
+
+  document.getElementById('reactapp'),
+);
